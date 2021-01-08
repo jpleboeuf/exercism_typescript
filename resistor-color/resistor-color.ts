9b59549c6cb19f69b7154758172a8a53969d68b1
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2021-01-09 00:45:23 +0100

    Solve exercise "Resistor Color"

--- resistor-color/resistor-color.ts
+++ resistor-color/resistor-color.ts
@@ -1,5 +1,18 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
+const CV: {[color: string]: number} = {
+  'black':   0,
+  'brown':   1,
+  'red':     2,
+  'orange':  3,
+  'yellow':  4,
+  'green':   5,
+  'blue':    6,
+  'violet' : 7,
+  'grey':    8,
+  'white':   9,
+};
+
+export function colorCode(color: string): number {
+  return CV[color];
 }
 
-export const COLORS = undefined
+export const COLORS: Array<string> = Object.keys(CV);
